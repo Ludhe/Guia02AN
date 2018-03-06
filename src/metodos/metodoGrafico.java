e8275af52c88c1c9b2eaa2513885934c5710d36c
villa <villa@192.168.0.11>
2018-03-05 20:01:48 -0600

    funciona la grafica

--- src/metodos/metodoGrafico.java
+++ src/metodos/metodoGrafico.java
@@ -5,10 +5,12 @@
  */
 package metodos;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.Marker;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
@@ -21,11 +23,11 @@ import org.jfree.data.xy.XYSeriesCollection;
  */
 public class metodoGrafico {
 
-    XYSeries serie1, serie2, serie3;
+    XYSeries serie1;
     XYSeriesCollection dataset;
     JFreeChart chart;
     ChartPanel panel;
-    XYPlot pt;
+    XYPlot plot;
     XYLineAndShapeRenderer render;
     
 
@@ -34,46 +36,34 @@ public class metodoGrafico {
 
     public ChartPanel graficar(double inicio, double fin) {
         serie1 = new XYSeries("funcion");
-        serie2 = new XYSeries("X");
-        serie3 = new XYSeries("Y");
         dataset = new XYSeriesCollection();
         render = new XYLineAndShapeRenderer();
 
         for (double i = inicio; i <= fin; i += 0.1) {
             serie1.add(i, funcion(i));
         }
-        serie2.add(-100, 0);
-        serie2.add(100, 0);
-        serie3.add(0, -100);
-        serie3.add(0, 100);
-
+        
         dataset.addSeries(serie1);
-        dataset.addSeries(serie2);
-        dataset.addSeries(serie3);
         chart = ChartFactory.createXYLineChart("", "X", "Y", dataset, PlotOrientation.VERTICAL, false, true, false);
         panel = new ChartPanel(chart);
 
-        pt = chart.getXYPlot();
-        
+        plot = chart.getXYPlot();
         
         render.setSeriesShapesVisible(0, Boolean.FALSE);
-        render.setSeriesShapesVisible(1, Boolean.FALSE);
-        render.setSeriesShapesVisible(2, Boolean.FALSE);
         render.setSeriesPaint(0, Color.RED);
-        render.setSeriesPaint(1, Color.BLACK);
-        render.setSeriesPaint(2, Color.BLACK);
-        render.setDrawOutlines(true);
-        render.setItemLabelsVisible(Boolean.TRUE);
         
         panel.setDomainZoomable(true);
+        panel.setRangeZoomable(true);
         panel.setMouseWheelEnabled(true);
         panel.setHorizontalAxisTrace(true);
         panel.setVerticalAxisTrace(true);
-        pt.setBackgroundPaint(Color.WHITE);
-        pt.setDomainPannable(true);
-        pt.setRangePannable(true);
-        pt.setRenderer(render);
-
+        plot.setBackgroundPaint(Color.WHITE);
+        plot.setDomainPannable(true);
+        plot.setRangePannable(true);
+        plot.setDomainZeroBaselineVisible(true);
+        plot.setRangeZeroBaselineVisible(true);
+        
+        plot.setRenderer(render);
         return panel;
     }
 

0ad8b6228330c1400c139502fcd52238c1aaaa2f
villa <villa@192.168.0.11>
2018-03-11 15:20:25 -0600

    se termimo la interfaz xd

--- src/metodos/MetodoGrafico.java
+++ src/metodos/MetodoGrafico.java
@@ -5,12 +5,14 @@
  */
 package metodos;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartMouseEvent;
+import org.jfree.chart.ChartMouseListener;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.Marker;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
@@ -21,7 +23,7 @@ import org.jfree.data.xy.XYSeriesCollection;
  *
  * @author villa
  */
-public class metodoGrafico {
+public class MetodoGrafico {
 
     XYSeries serie1;
     XYSeriesCollection dataset;
@@ -29,9 +31,9 @@ public class metodoGrafico {
     ChartPanel panel;
     XYPlot plot;
     XYLineAndShapeRenderer render;
-    
+    Robot robot;
 
-    public metodoGrafico() {
+    public MetodoGrafico() {
     }
 
     public ChartPanel graficar(double inicio, double fin) {
@@ -42,32 +44,50 @@ public class metodoGrafico {
         for (double i = inicio; i <= fin; i += 0.1) {
             serie1.add(i, funcion(i));
         }
-        
+
         dataset.addSeries(serie1);
-        chart = ChartFactory.createXYLineChart("", "X", "Y", dataset, PlotOrientation.VERTICAL, false, true, false);
+        chart = ChartFactory.createXYLineChart("", "", "", dataset, PlotOrientation.VERTICAL, false, true, false);
         panel = new ChartPanel(chart);
 
         plot = chart.getXYPlot();
-        
+
         render.setSeriesShapesVisible(0, Boolean.FALSE);
         render.setSeriesPaint(0, Color.RED);
-        
+
         panel.setDomainZoomable(true);
         panel.setRangeZoomable(true);
         panel.setMouseWheelEnabled(true);
         panel.setHorizontalAxisTrace(true);
         panel.setVerticalAxisTrace(true);
+        panel.addChartMouseListener(new ChartMouseListener() {
+
+            @Override
+            public void chartMouseMoved(ChartMouseEvent event) {
+                try {
+                    robot = new Robot();
+                    robot.keyPress(KeyEvent.VK_CONTROL);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+
+            @Override
+            public void chartMouseClicked(ChartMouseEvent event) {
+            }
+        });
+
         plot.setBackgroundPaint(Color.WHITE);
         plot.setDomainPannable(true);
         plot.setRangePannable(true);
         plot.setDomainZeroBaselineVisible(true);
         plot.setRangeZeroBaselineVisible(true);
-        
+
         plot.setRenderer(render);
         return panel;
     }
 
     public double funcion(double i) {
-        return (Math.exp(i - 1)) - (1.5 * i);
+        return i*i;
+//        return (Math.exp(i - 1)) - (1.5 * i);
     }
 }
